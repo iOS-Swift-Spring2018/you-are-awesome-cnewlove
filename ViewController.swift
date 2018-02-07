688d627c677aa1bef7be478706b9ce04035f5881
cnewlove <35679456+cnewlove@users.noreply.github.com>
2018-02-06 20:27:10 -0500

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -1,93 +1,56 @@
 //
 //  ViewController.swift
-//  You Are Awesome Version 3
+//  Word Garden
 //
-//  Created by Clark Newlove on 2/2/18.
+//  Created by Clark Newlove on 2/5/18.
 //  Copyright © 2018 Newlove. All rights reserved.
 //
 
 import UIKit
-import AVFoundation
 
 class ViewController: UIViewController {
 
-    // MARK: - Properties
-    @IBOutlet weak var awesomeImage: UIImageView!
-    @IBOutlet weak var messageLabel: UILabel!
-    @IBOutlet weak var soundSwitch: UISwitch!
-    var awesomePlayer = AVAudioPlayer()
-    var index = -1
-    var imageNumber = -1
-    var soundNumber = -1
-    var lastSound = -1
-    let numberOfImages = 5
-    let numberOfSounds = 3
-    var soundName = ""
+    @IBOutlet weak var userGuessLabel: UILabel!
+    @IBOutlet weak var guessedLetterField: UITextField!
+    @IBOutlet weak var theGuessCountLabel: UILabel!
+    @IBOutlet weak var guessLetterButton: UIButton!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-    }
-    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
-        if let sound = NSDataAsset(name: soundName) {
-            do {
-                try audioPlayer = AVAudioPlayer(data: sound.data)
-                audioPlayer.play()
-            } catch {
-                print("ERROR: data in\(soundName) couldn't be played as a sound.")
-            }
-        } else {
-            print("ERROR: file \(soundName) didn't load")
-        }
+        guessLetterButton.isEnabled = false
+        playAgainButton.isHidden = true
     }
 
-    func nonRepeatingRandom(lastNumber: Int, maxValue: Int) -> Int {
-        var newIndex = -1
-        repeat {
-            newIndex = Int(arc4random_uniform(UInt32(maxValue)))
-        } while index == newIndex
-        return newIndex
+    func updateUIAfterGuess () {
+        guessedLetterField.resignFirstResponder()
+        guessedLetterField.text = ""}
+    
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        if let letterGuessed = guessedLetterField.text?.last {
+            guessedLetterField.text = "\(letterGuessed)"
+            guessLetterButton.isEnabled = true
+        } else {
+            guessLetterButton.isEnabled = false
     }
-    // MARK: - Actions
+        
     
-    
-    @IBAction func soundSwitchPressed(_ sender: UISwitch) {
-        if !soundSwitch.isOn && soundNumber != -1 {
-            if soundNumber != -1 {
-                if lastSound != -1 {
-            awesomePlayer.stop()
-    
-            }
-        }
-    }
 }
-    @IBAction func messageButtonPressed(_ sender: UIButton!) {
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        updateUIAfterGuess()
         
-        let messages = ["You Are Fantastic!",
-                      "You Are Great!",
-                      "You Are Amazing!",
-                      "When the Genius Bar needs help, they call you!",
-                      "You Brighten My Day!",
-                      "You Are Da Bomb!",
-                      "I can't wait to use your app!",
-                      "Fabulous? That's You!"]
-        
-        index = nonRepeatingRandom(lastNumber: index, maxValue: messages.count)
-        messageLabel.text = messages[index]
-        
-        awesomeImage.isHidden = false
-        
-        imageNumber = nonRepeatingRandom(lastNumber: imageNumber, maxValue: numberOfImages)
-        awesomeImage.image = UIImage(named: "image\(imageNumber)")
-        
-        if soundSwitch.isOn {
-            soundNumber = nonRepeatingRandom(lastNumber: soundNumber, maxValue: numberOfSounds)
-            
-            let soundName = "sound\(soundNumber)"
-            playSound(soundName: soundName, audioPlayer: &awesomePlayer)
-        }
-        
-        
+    }
+    
+    @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+       updateUIAfterGuess()
+    }
+    
+    @IBAction func playAgainButtonPressed(_ sender: Any) {
     }
     
 }
+
+
+
